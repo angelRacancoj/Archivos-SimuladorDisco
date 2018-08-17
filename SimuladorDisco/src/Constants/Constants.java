dc8fd7c7b576add5f0124502a5f717ab7163a0f3
angelRacancoj <the1999evilrg@gmail.com>
2018-08-16 23:11:44 -0600

    Add files via upload

---
+++ SimuladorDisco/src/Constants/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Constants;
+
+/**
+ *
+ * @author angel
+ */
+public class Constants {
+
+    public final int PARTICION_INDEXADA = 1;
+    public final int PARTICION_CONTIGUA = 2;
+    public final int PARTICION_ENLAZADA = 3;
+
+}
