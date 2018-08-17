428daf8dfc8abf064d6d07df330e6f9f10902d04
edvinteodoro <edvinteodoro2@gmail.com>
2018-08-17 14:31:51 -0600

    Prueba

--- SimuladorDisco/src/Run/Run.java
+++ SimuladorDisco/src/Run/Run.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Run;
-
-/**
- *
- * @author angel
- */
+//Run
+//Run
 public class Run {
 
     /**
@@ -16,6 +8,7 @@ public class Run {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int entero;
     }
 
 }
